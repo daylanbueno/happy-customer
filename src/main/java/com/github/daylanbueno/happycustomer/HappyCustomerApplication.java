a424567511eb44d4ab679bb15c95d0d7e9f35069
DAILAN BUENO DOS SANTOS <x04501348143@tcu.gov.br>
2022-05-20 18:41:53 -0300

    start project

---
+++ src/main/java/com/github/daylanbueno/happycustomer/HappyCustomerApplication.java
@@ -0,0 +1,13 @@
+package com.github.daylanbueno.happycustomer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HappyCustomerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HappyCustomerApplication.class, args);
+	}
+
+}
---
+++ src/main/java/com/github/daylanbueno/happycustomer/entity/Customer.java
@@ -0,0 +1,6 @@
+package com.github.daylanbueno.happycustomer.entity;
+
+public class Customer {
+    private Integer id;
+    private String name;
+}
