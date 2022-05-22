65b60521d1d1ff9c2ac67c561f3a3d7a0f3d2616
DAILAN BUENO DOS SANTOS <x04501348143@tcu.gov.br>
2022-05-22 12:21:59 -0300

    incluid transaction initial

---
+++ src/main/java/com/github/daylanbueno/happycustomer/domain/dto/GroupDto.java
@@ -0,0 +1,13 @@
+package com.github.daylanbueno.happycustomer.domain.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class Group {
+    private String moth;
+    private Integer totalPoint;
+}
