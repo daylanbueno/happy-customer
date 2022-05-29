180466b9e70bc7bc6d9292d4ccd2705bb6c30a29
DAILAN BUENO DOS SANTOS <x04501348143@tcu.gov.br>
2022-05-29 19:53:17 -0300

    configuration swagger to documentation

---
+++ src/main/java/com/github/daylanbueno/happycustomer/config/SwaggerConfig.java
@@ -0,0 +1,44 @@
+package com.github.daylanbueno.happycustomer.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import springfox.documentation.builders.ApiInfoBuilder;
+import springfox.documentation.builders.PathSelectors;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.service.ApiInfo;
+import springfox.documentation.service.Contact;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
+@EnableSwagger2
+@Configuration
+public class SwaggerConfig {
+
+    @Bean
+    public Docket docket() {
+        return new Docket(DocumentationType.SWAGGER_2)
+                .select()
+                .apis(RequestHandlerSelectors.basePackage("com.github.daylanbueno.happycustomer.resource"))
+                .paths(PathSelectors.any())
+                .build()
+                .apiInfo(apiInfo());
+    }
+
+    private Contact contact() {
+        return new Contact(
+                "Dailan Bueno",
+                "https://github.com/daylanbueno",
+                "daylansantos@gmail.com"
+        );
+    }
+
+    private ApiInfo apiInfo() {
+        return new ApiInfoBuilder()
+                .title("Library API")
+                .description("API para controle de livraria, aluguel de livros")
+                .version("1.0")
+                .contact(contact())
+                .build();
+    }
+}
